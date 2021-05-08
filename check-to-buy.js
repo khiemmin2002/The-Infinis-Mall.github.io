31d6d5a4728a6857aa3dc4535aa70e772bd5e0c8
Giang Nhật Khánh <73809007+NhatKhanhGiang@users.noreply.github.com>
2021-05-08 22:08:13 +0700

    Update check-to-buy.js

--- check-to-buy.js
+++ check-to-buy.js
@@ -3,9 +3,11 @@ var addToCartBtn = document.getElementById("add");
 
 addToCartBtn.onclick = () => {
     if (sessionStorage.getItem("loginSuccessful")) {
+        return true;
     }
     else {
         addToCartBtn.href = "javascript:void(0)";
         alert("You have to log in");
+        return false;
     }
 }
