bb41fce8cb13b4345c28f7b74de4b6d066151488
tranhoangphuc2001 <53502486+tranhoangphuc2001@users.noreply.github.com>
2021-05-08 17:20:44 +0700

    Update Registration.js

--- Registration.js
+++ Registration.js
@@ -15,6 +15,8 @@ function validation(){
 
     const email_regex = /^([a-zA-Z0-9\.-]{3,})@([a-zA-Z0-9\.-]+).([a-z]{2,5})$/;
         
+    //Email Validation
+    
     if (!email_regex.test(email)){
         text = "Please Enter Valid Email";
         error_message.innerHTML = text;
@@ -23,30 +25,40 @@ function validation(){
 
     const phone_regex = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{3,5})$/;
 
+    //Phone Validation
+    
     if (!phone_regex.test(phone)){
         text = "Please Enter Valid Phone Number";
         error_message.innerHTML = text;
         return false;
     }
 
+    //First Name Validation
+    
     if (firstname.length < 3){
         text = "Please Enter Valid First Name";
         error_message.innerHTML = text;
         return false;
     }
 
+    //Last Name Validation
+    
     if (lastname.length < 3){
         text = "Please Enter Valid Last Name";
         error_message.innerHTML = text;
         return false;
     }
 
+    //Address Validation
+    
     if (address.length < 3){
         text = "Please Enter Valid Address";
         error_message.innerHTML = text;
         return false;
     }
 
+    //City Validation
+    
     if (city.length < 3){
         text = "Please Enter Valid City";
         error_message.innerHTML = text;
@@ -55,12 +67,16 @@ function validation(){
 
     const zip_regex = /^([0-9]{4,6})$/
 
+    //Zip Number Validation
+    
     if (!zip_regex.test(zip)){
         text = "Please Enter Valid Zip Number";
         error_message.innerHTML = text;
         return false;
     }
 
+    //Password Validation
+    
     if (password.length < 8){
         text = "Password Too Short (8 to 20 characters)";
         error_message.innerHTML = text;
@@ -103,9 +119,13 @@ function validation(){
         return false;
     }
 
+    //Form Submit
+    
     alert("Form Submitted");
     return true;
 }
+
+//Function Shows Radio Store Owner
 
 function showForm(x){
     if (x==0){
